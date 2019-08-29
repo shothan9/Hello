6eb4e6ba9747098134ebf3b8aa84b0c21671894c
shothan9 <54327596+shothan9@users.noreply.github.com>
2019-08-29 11:11:40 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/* Author: Sam Hothan
+** Date: August 29 2019
+** A simple Hello World! Program
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main {
+
+ printf("Sam Hothan"\n);
+
+ return 0;
+}
